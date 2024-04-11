c27e1ade03cd0c1c7988696337f42468f8c7db14
AnushkaShorewala <157656371+AnushkaShorewala@users.noreply.github.com>
2024-04-11 17:30:41 +0000

    done!

---
+++ scripts/cookie-banner.js
@@ -0,0 +1,14 @@
+
+console.log("hello from cookie-banner.js")
+
+$("cookie-accept-button").click(
+
+    function() {
+
+    console.log("cookie banner button clicked");
+
+    $("#cookie-banner").addClass("hidden");
+
+}
+
+);
